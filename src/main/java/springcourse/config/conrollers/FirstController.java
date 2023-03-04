13647f6410b14195dd579fdd9a81ce9cd28f260d
Tanya Serbinenko <tanyaserbinenko1996@gmail.com>
2023-03-04 15:29:02 +0200

    init

---
+++ src/main/java/springcourse/config/conrollers/FirstController.java
@@ -0,0 +1,17 @@
+package springcourse.config.conrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//@RequestMapping("first")
+public class FirstController {
+    @GetMapping("/hello")
+     public String helloPage (){
+        return "first/hello";
+     }
+     @GetMapping("/goodbye")
+     public String goodbyePage (){
+        return "first/goodbye";
+     }
+}
